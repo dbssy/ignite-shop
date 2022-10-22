05d9be559dc52b62a5a3abb3927ff56966f0ef1a
João Debussy <social.debussy@gmail.com>
2022-10-22 15:00:03 -0300

    criando header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { AppProps } from 'next/app';
 
 import { globalStyles } from '../styles/global';
+import { Container, Header } from '../styles/pages/app';
+
+import logoImg from '../assets/logo.svg';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles();
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
 
-  return <Component {...pageProps} />
+      <Component {...pageProps} />
+    </Container>
+  );
 }
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import { styled } from '../styles';
-
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-})
-
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <h1>Olá</h1>
   );
 }
